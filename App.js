03482adb615f7f7cb95a268a00dabc16b5040c44
toofaaan <168278782+toofaaan@users.noreply.github.com>
2024-09-23 22:28:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Home from './Component/Home';
+import Key from './Component/Key';
+import Navbar from './Component/Navbar';
+import Work from './Component/Work';
+
+function App() {
+  return (
+    <>
+
+    <Navbar/>
+    <Home/>
+    <Key/>
+    <Work/>
+      
+    </>
+  );
+}
+
+export default App;
